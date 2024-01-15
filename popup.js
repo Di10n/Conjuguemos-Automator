68580e2c632f3d42a148c29e8b61e3827f854a84
Di10n <dilonchugh@gmail.com>
2024-01-14 23:51:16 -0800

    time framework - not functional

--- popup.js
+++ popup.js
@@ -31,11 +31,64 @@ function clickListen() {
     });
 }
 
+var timewarning = [0, 0, 0];
+
+function checkTime() {
+  let warning = document.getElementById("timewarning");
+  if ((timewarning[0] || timewarning[1]) && timewarning[2]) {
+    warning.style.display = "inline";
+  } else {
+    warning.style.display = "none";
+  }
+}
+
+function checkListen() {
+  var label = document.getElementById("SaveLabel")
+  var time = document.getElementById("timerbox")
+  document.getElementById("SaveScore").addEventListener('change', function() {
+    if (this.checked) {
+      label.style.color = "black";
+      time.style.display = "inline"
+      timewarning[2] = 1;
+    } else {
+      label.style.color = "#5f6367";
+      time.style.display = "none"
+      timewarning[2] = 0;
+    }
+    checkTime();
+  });
+}
+
+function minHandler() {
+  let value = document.getElementById("min").value;
+  if (value > 59) {
+    timewarning[0] = 1;
+  } else if (value <= 59) {
+    timewarning[0] = 0;
+  }
+  checkTime();
+}
+
+function secHandler() {
+  let value = document.getElementById("sec").value;
+  if (value > 59) {
+    timewarning[1] = 1;
+  } else if (value <= 59) {
+    timewarning[1] = 0;
+  }
+  checkTime();
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("warning").style.display = "none";
   var button = document.getElementById("start-btn");
   button.addEventListener("click", clickHandler);
   var input = document.getElementById("number");
   input.addEventListener("input", inputHandler);
+  var min = document.getElementById("min");
+  min.addEventListener("input", minHandler);
+  var sec = document.getElementById("sec");
+  sec.addEventListener("input", secHandler);
   clickListen();
+  checkListen();
 });
